daea3aa866f8f8170c09bfb6180aef2965253eb9
304089 <9457imai2@gmail.com>
2021-12-13 17:56:41 +0900

    [create]ログアウト機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,7 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def 
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 end
