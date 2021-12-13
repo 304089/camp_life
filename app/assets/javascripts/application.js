c82d75e4ad88b6dca572b8056269c54356dd38a8
304089 <9457imai2@gmail.com>
2021-12-14 00:02:51 +0900

    [create]ギア登録、編集機能

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,67 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+$(function() {
+  const schedule = $(".plan_schedule");
+  const gear = $(".plan_gear");
+
+  $(function() {
+    $(".detail_choice__schedule").on("click", function() {
+      if ($(schedule).hide()) {
+        $(schedule).fadeIn();
+        $(gear).hide();
+        $(".detail_choice__schedule").css("background", "white")
+        $(".detail_choice__gear").css("background", "silver")
+      }
+    });
+  });
+
+  $(function() {
+    $(".detail_choice__gear").on("click", function() {
+      if ($(gear).hide()) {
+        $(gear).fadeIn();
+        $(schedule).hide();
+        $(".detail_choice__schedule").css("background", "silver")
+        $(".detail_choice__gear").css("background", "white")
+      }
+    });
+  });
+
+  $(function(){
+    const menu = $(".menu_list")
+    $(".menu_btn").on("click", function() {
+      if ($(".menu_list").hasClass("menu_active")) {
+        $(menu).removeClass("menu_active");
+        $(menu).animate({'right':'0'},300);
+        $(".main").css("background-color", "rgba(0,0,0,0.4)");
+        $(".close_btn").removeClass("menu_btn_hide");
+        $(".open_btn").addClass("menu_btn_hide");
+
+      }else{
+        $(menu).addClass("menu_active");
+        $(menu).animate({'right':'-100%'},300);
+        $(".main").css("background-color", "transparent");
+        $(".close_btn").addClass("menu_btn_hide");
+        $(".open_btn").removeClass("menu_btn_hide");
+      }
+    });
+  });
+});
+
+$(function() {
+  $('#gear_gear_image').on('change', function (e) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      $(".change_file").attr('src', e.target.result);
+    }
+    reader.readAsDataURL(e.target.files[0]);
+  });
+  $('#user_profile_image').on('change', function (e) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      $(".change_file").attr('src', e.target.result);
+    }
+    reader.readAsDataURL(e.target.files[0]);
+  });
+});
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :login_user, only:[:new, :create]
   before_action :set_user, except:[:new, :create]
 
   def new
@@ -9,7 +10,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to user_path(@user)
     else
       render "new"
     end
